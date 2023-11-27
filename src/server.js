9054cb4880b6d4a1be991b8f6bad2327513ddef5
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-11-27 15:32:39 +0700

    config Template Engine EJS and dotenv

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express"); // common js
+const path = require("path"); // common js
+// import express from "express";  // es module
+require("dotenv").config();
+
+const app = express(); // app express
+const port = process.env.PORT || 8081;
+const hostname = process.env.HOST_NAME || "localhost";
+
+console.log(process.env);
+
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Khai báo routes
+app.get("/", (req, res) => {
+    res.send("Hello World from CiKey!");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`);
+});
