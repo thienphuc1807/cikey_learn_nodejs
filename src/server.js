44efa41c94e59a610d1a2fa4a3d7271a88167f3d
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-11-29 14:57:27 +0700

    Config Connection

--- src/server.js
+++ src/server.js
@@ -2,23 +2,13 @@ require("dotenv").config();
 const express = require("express"); // common js
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-// get the client
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8081;
 const hostname = process.env.HOST_NAME || "localhost";
 
 // console.log(process.env);
-
-// create the connection to database
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    user: "root",
-    password: "123456",
-    database: "hoidanit",
-});
 
 connection.query("SELECT * FROM Users u", function (err, results, fields) {
     console.log(results); // results contains rows returned by server
