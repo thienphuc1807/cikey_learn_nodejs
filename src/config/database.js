f781935ea61d0a19adbe79a1da53fbc6871ebb7c
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-11-30 07:35:38 +0700

    Connection Pool

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 // create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST, // string
     port: process.env.DB_PORT, // number
     user: process.env.DB_USER, // string
     password: process.env.DB_PASSWORD, // string
     database: process.env.DB_NAME, // string
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
 });
 
 module.exports = connection;
---
+++ src/config/oldDB.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+// get the client
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST, // string
+    port: process.env.DB_PORT, // number
+    user: process.env.DB_USER, // string
+    password: process.env.DB_PASSWORD, // string
+    database: process.env.DB_NAME, // string
+});
+
+module.exports = connection;
