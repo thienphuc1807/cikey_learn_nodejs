b913af47c16731b1b1054abf6077a6327dd5c1aa
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-11-30 14:08:50 +0700

    Add new User into database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,23 @@
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
     return res.render("home");
 };
 
+// add new user into database
 const addNewUser = (req, res) => {
-    console.log(req.body);
-    return res.send("add new user");
+    let name = req.body.name;
+    let email = req.body.email;
+    let city = req.body.city;
+    connection.query(
+        `INSERT INTO Users (email,name,city)
+        VALUES (?, ?, ?);`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+        }
+    );
+    return res.send("Success");
 };
 
 module.exports = {
