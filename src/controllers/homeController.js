22a2be3e3f8f11dbf26f31882d4744b808312d77
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-11-30 15:21:05 +0700

    Create New User Page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,23 +4,35 @@ const getHomePage = (req, res) => {
     return res.render("home");
 };
 
+const getCreateUserPage = (req, res) => {
+    return res.render("createUser");
+};
+
 // add new user into database
-const addNewUser = (req, res) => {
+const addNewUser = async (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     let city = req.body.city;
-    connection.query(
-        `INSERT INTO Users (email,name,city)
-        VALUES (?, ?, ?);`,
-        [email, name, city],
-        function (err, results) {
-            console.log(results);
-        }
+
+    // connection.query(
+    //     `INSERT INTO Users (email,name,city)
+    //     VALUES (?, ?, ?);`,
+    //     [email, name, city],
+    //     function (err, results) {
+    //         console.log(results);
+    //     }
+    // );
+
+    const promisePool = connection.promise();
+    const [rows, fields] = await promisePool.query(
+        `INSERT INTO Users (email,name,city) VALUES (?, ?, ?)`,
+        [email, name, city]
     );
-    return res.send("Success");
+    res.send("Success");
 };
 
 module.exports = {
     getHomePage,
     addNewUser,
+    getCreateUserPage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getHomePage, addNewUser } = require("../controllers/homeController");
+const {
+    getHomePage,
+    addNewUser,
+    getCreateUserPage,
+} = require("../controllers/homeController");
 
 // router.method(path,handler)
 router.get("/", getHomePage);
-
+router.get("/create", getCreateUserPage);
 router.post("/create-new-user", addNewUser);
 
 module.exports = router;
